2fa358ad10600591b4091f73ad6693d8963de358
Tomasz Zielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-12-23 15:15:16 +0100

    corrections

--- experience.js
+++ experience.js
@@ -4,21 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
 						exp.style.color = this.value;
 					});
 				}
-		
-				/* poniżej to samo z użyciem przycisków:
-				document.querySelectorAll('button').forEach(button => {
-					button.onclick = () => {
-						document.querySelector('#hello').style.color = button.dataset.color;
-					}
-				});
-				lub
-				document.querySelector('#red').onclick = function() {
-					document.querySelector('#hello').style.color = 'red';
-				}
-				document.querySelector('#blue').onclick = function() {
-					document.querySelector('#hello').style.color = 'blue';
-				}
-				document.querySelector('#green').onclick = function() {
-					document.querySelector('#hello').style.color = 'green';
-				}*/
+					
 			});
